1812753c529de534d08c97503a10032058e574af
西大樹 <nishidaiki@nishidaikinoMacBook-Pro-2.local>
2024-08-11 23:20:50 +0900

    Second commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $query = Product::query();
+        $search = $request->search;
+        $company_id = $request->input('company_id');
+
+
+
+        if($search){
+            $query->where('product_name', 'LIKE', "%".$search."%");
+        }
+
+        if($company_id){
+            $query->where('company_id', $company_id);
+        }
+
+
+
+        $products = $query->paginate(10);
+
+        $companies = Company::whereNull('deleted_at')->get();
+
+
+        // $companies = Company::whereDoesntHave('products', function ($query) {
+        //     $query->whereNotNull('deleted_at');
+        // })->get();
+
+        return view('products.index', compact('products','companies','company_id','search'));
+    }
+
+
+
+
+
+
+    public function create()
+    {
+        $companies = Company::all();
+        //
+        return view('products.create', compact('companies'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'product_name' => 'required',
+            'company_id' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'comment' => 'nullable',
+            'img_path' => 'nullable|image',
+        ]);
+
+        // $data = $request->all();
+        // $data['comment'] = $request->input('comment', null);
+        // $data['img_path'] = $request->input('img_path', null);
+
+        $product = new Product([
+            'product_name' => $request->get('product_name'),
+            'company_id' => $request->get('company_id'),
+            'price' => $request->get('price'),
+            'stock' => $request->get('stock'),
+            'comment' => $request->get('comment'),
+        ]);
+
+        if($request->hasFile('img_path')){
+            $filename = $request->img_path->getClientOriginalName();
+            $filePath = $request->img_path->storeAs('products',$filename,'public');
+            $product->img_path = '/storage/' . $filePath;
+        }
+
+        $product->save();
+
+        return redirect('products');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return view('products.show',['product' => $product]);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        $companies = Company::all();
+
+        return view('products.edit', compact('product', 'companies'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'product_name' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+        ]);
+
+        $product->product_name = $request->product_name;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+
+        $product->save();
+
+        return redirect()->route('products.index')
+           ->with('success', 'product updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $company = $product->company;
+
+        $product->delete();
+
+        if($company->products()->count() === 0) {
+            $company->delete();
+        }
+
+        return redirect('/products');
+    }
+}
---
+++ app/Models/Company.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    public function products()
+    {
+         return $this->hasMany(Product::class);
+    }
+}
